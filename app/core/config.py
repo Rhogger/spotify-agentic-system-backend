927e726d4f05d4b1da18e647e9d22a58d67c8902
Rhogger <rhogger.silva@unirv.edu.br>
2026-01-22 10:46:50 -0300

    feat: adicionado novas envs e novos campos no User

--- app/core/config.py
+++ app/core/config.py
@@ -11,6 +11,12 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str
 
+    SPOTIFY_CLIENT_ID: str
+    SPOTIFY_CLIENT_SECRET: str
+    SPOTIFY_REDIRECT_URI: str
+
+    SECRET_KEY: str
+
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", case_sensitive=True
     )
--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,12 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 
+
 class User(Base):
     __tablename__ = "users"
 
     id: Mapped[str] = mapped_column(primary_key=True)
     display_name: Mapped[str | None]
-    email: Mapped[str | None]
+    spotify_id: Mapped[str | None]
+    spotify_access_token: Mapped[str | None]
+    spotify_refresh_token: Mapped[str | None]
